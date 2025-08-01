2a2c7b2468aa1a8a9e33e7a848b61518a7b34674
Ashok Saravanan <ashko.saravanan@gmail.com>
2025-08-01 15:30:22 -0500

    more setup

--- server/app/db.py
+++ server/app/db.py
@@ -1,19 +1,20 @@
 import os
 from typing import Generator
+
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy.pool import NullPool
 
 # Load environment variables from .env
 load_dotenv()
 
 # Fetch variables
-user = os.getenv("user")
-password = os.getenv("password")
-host = os.getenv("host")
-port = os.getenv("port")
-dbname = os.getenv("dbname")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
+dbname = os.getenv("DB_NAME")
 
 # Construct the SQLAlchemy connection string
 database_url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}?sslmode=require"
