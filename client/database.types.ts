53703b26d87559942350c70fd717beb235daf329
siladiea <siladiea2005@gmail.com>
2025-09-29 15:09:08 -0400

    Landing Page Changes

--- client/database.types.ts
+++ client/database.types.ts
@@ -183,6 +183,30 @@ export type Database = {
           },
         ]
       }
+      email_subscriptions: {
+        Row: {
+          created_at: string | null
+          email: string
+          id: string
+          status: string | null
+          updated_at: string | null
+        }
+        Insert: {
+          created_at?: string | null
+          email: string
+          id?: string
+          status?: string | null
+          updated_at?: string | null
+        }
+        Update: {
+          created_at?: string | null
+          email?: string
+          id?: string
+          status?: string | null
+          updated_at?: string | null
+        }
+        Relationships: []
+      }
       fields: {
         Row: {
           created_at: string | null
